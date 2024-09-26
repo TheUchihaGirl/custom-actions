a8276d5645b6e76e427b8ec28ec7683e90d2a136
= <=>
2024-09-27 01:37:04 +0530

    fixed error

--- .github/actions/addition/index.js
+++ .github/actions/addition/index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 
 function run() {
-    let firstNumber = core.getInput('firstNumber');
-    let secondNumber = core.getInput('secondNumber');
+    let firstNumber = core.getInput('number1');
+    let secondNumber = core.getInput('number2');
 
     let result = parseInt(firstNumber) + parseInt(secondNumber);
     core.notice('The result is: ' + result);
